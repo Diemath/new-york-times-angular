a87586139175e68fa9f2e6654ede3befa22a7218
Dmytro Yehorov <mrdiemath@outlook.com>
2019-06-10 15:23:41 +0300

    Add simple navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav/nav-bar.component';
 
 import {
   NewsListComponent,
@@ -16,6 +17,7 @@ import {
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     NewsListComponent,
     NewsThumbnailComponent
   ],
